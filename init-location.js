a5becbc83add6c2101e16c669a55ec673f48fb0e
Rajeshwari-Rudra <rajeshwarihyd.02@gmail.com>
2021-01-24 20:05:43 -0600

    initial commit

---
+++ init-location.js
@@ -0,0 +1,45 @@
+
+export default [
+    {
+        Name: 'B.D. Owens Library One',
+        North: 20.344208,
+        West: -34.891494,
+        South: 20.343652,
+        East: -54.8884,
+        Notes: 'tower',
+        Attachments: '',
+        'Quest-Location': 'Library',
+    },
+    {
+        Name: 'Stuent',
+        North: 79.344208,
+        West: -84.891494,
+        South: 90.343652,
+        East: -64.8884,
+        Notes: 'tower',
+        Attachments: '',
+        'Quest-Location': 'Stuent Union',
+
+    },
+    {
+        Name: 'Admin Building',
+        North: 10.344208,
+        West: -84.891494,
+        South: 30.343652,
+        East: -74.8884,
+        Notes: 'tower',
+        Attachments: '',
+        'Quest-Location': 'Admin Building',
+    },
+
+    {
+        Name: 'Colden Pond',
+        North: 40.350438,
+        West: -94.883363,
+        South: 40.350296,
+        East: -94.883180,
+        Notes: 'Colden Hall',
+        Attachments: '',
+        'Quest-Location': 'Colden Pond',
+    },
+];
