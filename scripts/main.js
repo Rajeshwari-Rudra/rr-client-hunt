f6fc31556057cc5af7730b6f317e787b20890281
Rajeshwari-Rudra <rajeshwarihyd.02@gmail.com>
2021-01-29 11:53:52 -0600

    add speech utterences

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,10 @@
-/**
- * main.js - the logic for our app
- * Get the location
- * @author Rajeshwari Rudravaram
- */
+// Resource : http://www.movable-type.co.uk/scripts/latlong.html?from=48.86,-122.0992&to=48.8599,-122.1449 
+// Above link for calculating distance 
 
-// first imports.......................
+
 import locationsArray from '../init-locations.js';
 
-// helper functions....................
-
-
-// event handlers......................
-
-
+// for location element we assigned location id.
 let locationElement = document.getElementById("location");
 
 window.addEventListener('load', main);
@@ -23,11 +15,12 @@ function main() {
     console.log('Page is fully loaded');
 }
 
-let currentlat;
-let currentlon;
+// initializing the current position latitude and longitude and error to true
+let currentPositionLatitude;
+let currentPositionLongitude;
 let error = true;
 
-// getLocation() function is used to collect the current location
+// collects current location
 async function getLocation() {
     return new Promise((resolve, reject) => {
         navigator.geolocation.getCurrentPosition(resolve, reject);
@@ -36,38 +29,38 @@ async function getLocation() {
     });
 }
 
-//the locationHandler() function checksout the current location and compares it with the 
-//init-locations.
-
+// gets current locations and compares the locations from init-locations.js
+// if error is true provides you're not 20 meters nearer to any place
+// if error is false won't show any thing
 async function locationHandler() {
     let locText = await getLocation();
-    currentlat = locText.coords.latitude;
-    document.getElementById("device-lat").innerHTML = "device-latitude: " + currentlat.toFixed(6);
-    currentlon = locText.coords.longitude;
-    document.getElementById("device-long").innerHTML = "device-longitude: " + currentlon.toFixed(6);
+    currentPositionLatitude = locText.coords.latitude;
+    document.getElementById("device-lat").innerHTML = "This is about device-lat: " + currentPositionLatitude.toFixed(6);
+    currentPositionLongitude = locText.coords.longitude;
+    document.getElementById("device-long").innerHTML = "This is about device-long: " + currentPositionLongitude.toFixed(6);
 
     locationsArray.forEach(function (value) {
         if (isInside(value.Latitude, value.Longitude)) {
             document.getElementById("locationAnswer").innerHTML = value.Name;
+            let utterance = new SpeechSynthesisUtterance("You reached the quest. Welcome to " + value.Name);
+            speechSynthesis.speak(utterance);
             error = false;
         }
     });
 
-    // In case of any error where if the device is not 30m range it displays error.
-
     if(error) {
-        document.getElementById("error-message").innerHTML = "You're not in the radius range.";
+        let innerHTML = "You're not 20 meters nearer to any place.";
+        document.getElementById("error-message").innerHTML = innerHTML;
+        let utterance = new SpeechSynthesisUtterance(innerHTML);
+        speechSynthesis.speak(utterance);
     } else {
         document.getElementById("error-message").innerHTML = "";
     }
 }
 
-
-//checking if distance is in 10m range.
-
-
-function isInside(questLat, questLon) {
-    let distance = distanceBetweenLocations(questLat, questLon);
+//Calculates distance and checks distance is below or above 20 meters
+function isInside(questLatitde, questLongitude) {
+    let distance = distanceBetweenLocations(questLatitde, questLongitude);
     console.log("distance: " + distance);
     if (distance < 20) {
         return true;
@@ -76,13 +69,15 @@ function isInside(questLat, questLon) {
     }
 }
 
-//distance between the lat-long points.
-function distanceBetweenLocations(questLat, questLon) {
+
+//Calculate distance between Latitude/Longitude points
+//Returns the  calculated distance in meters
+function distanceBetweenLocations(questLatitde, questLongitude) {
     const R = 6371e3;
-    const φ1 = currentlat * Math.PI / 180;
-    const φ2 = questLat * Math.PI / 180;
-    const Δφ = (questLat - currentlat) * Math.PI / 180;
-    const Δλ = (questLon - currentlon) * Math.PI / 180;
+    const φ1 = currentPositionLatitude * Math.PI / 180;
+    const φ2 = questLatitde * Math.PI / 180;
+    const Δφ = (questLatitde - currentPositionLatitude) * Math.PI / 180;
+    const Δλ = (questLongitude - currentPositionLongitude) * Math.PI / 180;
 
     const a = Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
         Math.cos(φ1) * Math.cos(φ2) *
@@ -90,5 +85,7 @@ function distanceBetweenLocations(questLat, questLon) {
     const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
 
     const d = R * c;
-    return d; 
+    return d; // in metres
 }
+
+
