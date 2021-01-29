b283a05b89b6564b2bbfdee5e52c814d96743acf
Rajeshwari-Rudra <rajeshwarihyd.02@gmail.com>
2021-01-29 12:07:40 -0600

    add new message

--- scripts/main.js
+++ scripts/main.js
@@ -42,7 +42,7 @@ async function locationHandler() {
     locationsArray.forEach(function (value) {
         if (isInside(value.Latitude, value.Longitude)) {
             document.getElementById("locationAnswer").innerHTML = value.Name;
-            let utterance = new SpeechSynthesisUtterance("You reached the quest. Welcome to " + value.Name);
+            let utterance = new SpeechSynthesisUtterance("Hey...! You have reached the destination Welcome to " + value.Name);
             speechSynthesis.speak(utterance);
             error = false;
         }
