03ba1edccde630c2e1065b39e27d53c4950d53c5
Rajeshwari-Rudra <rajeshwarihyd.02@gmail.com>
2021-01-31 17:44:14 -0600

    add location

--- init-locations.js
+++ init-locations.js
@@ -1,18 +1,24 @@
 export default [
+    
+    {
+        Name: 'Horizon Apartments',
+        Latitude: 40.3589695,
+        Longitude: -94.8831951
+    },
+    {
+        Name: 'Colden Pond',
+        Latitude: 40.3500419,
+        Longitude: -94.8851095
+    },
     {
         Name: 'B. D. Owens Library',
          Latitude: 40.3535922,
          Longitude:-94.888208
     },
     {
-        Name: 'Horizon Apartments',
-        Latitude: 40.3589695,
-        Longitude: -94.8831951
-    }
-    // {
-    //     Name: 'Colden Pond',
-    //     Latitude: 40.3500419,
-    //     Longitude: -94.8851095
-    // }
+        Name: 'Valk Center',
+         Latitude: 40.352821097563805,
+         Longitude:-94.88050461598701
+    } 
 
 ];
